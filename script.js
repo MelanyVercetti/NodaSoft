994250ddd2d9bf3175e07903ab3838102e169c02
Melany <melany777@mail.ru>
2022-01-26 15:04:50 +0600

    ready

---
+++ script.js
@@ -0,0 +1,11 @@
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+
+openBtn.addEventListener('click', () => {
+    modal.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.toggle('active');
+})
